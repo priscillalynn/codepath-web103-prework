2866be554711c2a9cb37233bacefb282c77fde34
priscillalynn <plynn9036@gmail.com>
2023-06-18 12:03:51 -0400

    cards closer to how they should look

--- src/components/content-creator/CreatorCard.js
+++ src/components/content-creator/CreatorCard.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import '../../../src/client'
+import './CreatorCard.css'
+import { BsInfoCircle } from 'react-icons/bs'
+import { MdModeEditOutline } from 'react-icons/md'
 
 const CreatorCard = ({ creator }) => {
+
   return (
     <>
+    <div className='creator-card-container'>
+    <div className="bg-card-image" style={{ backgroundImage: `url(${creator.imageURL})`, backgroundSize: 'cover',
+    backgroundPosition: 'center' }}>
     <div className='creator-card'>
-      <h3>{creator.name}</h3>
+      <h3 className='creator-name'>{creator.name}</h3>
+      <div className='icons'>
+        <BsInfoCircle />
+        <MdModeEditOutline />
+        </div>
       <p>{creator.description}</p>
       <p>{creator.url}</p>
       <p>{creator.imageURL}</p>
+      </div>
+      </div>
       </div>
     </>
     
